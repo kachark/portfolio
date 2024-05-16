5b10daab1b09b1e2be2a33fa85b30b6dc8ca0570
kachark <kkachar@hotmail.com>
2024-05-15 22:51:29 -0500

    fixed alignment with projects link

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,9 +10,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          Projects
-        </li>
+        <li><a href="{base}/">Projects</a></li>
         <li><a href="{base}/kachar_resume_2023.pdf">Resume</a></li>
       </ul>
     </div>
@@ -22,7 +20,7 @@
   </div>
   <div class="navbar-end">
     <ul class="menu menu-horizontal px-1 hidden md:flex">
-      <li>Projects</li>
+      <li><a href="{base}/">Projects</a></li>
       <li><a href="{base}/kachar_resume_2023.pdf">Resume</a></li>
     </ul>
     <div class="md:divider md:divider-horizontal hidden" />

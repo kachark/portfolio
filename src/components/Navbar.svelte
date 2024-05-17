86bd17600e75940b6a79ae515f70a020e5ad1e8e
kachark <kkachar@hotmail.com>
2024-05-16 21:26:45 -0500

    centers name on small breakpoint

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -9,14 +9,16 @@
       <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a href="{base}/">Projects</a></li>
         <li><a href="{base}/kachar_resume_2023.pdf">Resume</a></li>
       </ul>
     </div>
-    <a class="btn btn-ghost text-xl" href="{base}/" >Koray G. Kachar</a>
+    <a class="md:btn md:btn-ghost md:text-xl hidden" href="{base}/" >Koray G. Kachar</a>
   </div>
-  <div class="navbar-center hidden lg:flex">
+  <div class="navbar-center md:hidden">
+    <a class="btn btn-ghost text-xl mx-auto" href="{base}/" >Koray G. Kachar</a>
   </div>
   <div class="navbar-end">
     <ul class="menu menu-horizontal px-1 hidden md:flex">

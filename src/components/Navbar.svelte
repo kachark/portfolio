4a811e558fc00635cf346a53ee82b590dcb046da
kachark <kkachar@hotmail.com>
2024-05-18 18:20:42 -0500

    adjusted pixel height of resume icon

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -11,7 +11,7 @@
     <ul class="menu menu-horizontal px-1 hidden md:flex">
       <li><a href="{base}/kachar_resume_2023.pdf">Resume</a></li>
     </ul>
-    <a href="{base}/kachar_resume_2023.pdf"><Icon class="mr-2 md:hidden" icon="pepicons-pop:cv" width="24" height="24" /></a>
+    <a href="{base}/kachar_resume_2023.pdf"><Icon class="mr-2 md:hidden" icon="pepicons-pop:cv" width="24" height="22" /></a>
     <div class="md:divider md:divider-horizontal hidden" />
     <a class="mx-2" href="https://github.com/kachark"><Icon icon="mdi:github" width="24" height="24" /></a>
     <a class="mx-2" href="https://www.linkedin.com/in/koray-kachar/"><Icon icon="mdi:linkedin" width="24" height="24" /></a>

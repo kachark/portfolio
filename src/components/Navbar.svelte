9e68471d54fe2a0b98a904b7555b4da4a8d98c16
kachark <kkachar@hotmail.com>
2024-05-18 18:14:15 -0500

    moved name and resume icon for small displays to improve layout

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,18 +5,15 @@
 
 <div class="navbar p-6 shadow-sm max-h-20">
   <div class="navbar-start">
-    <a href="{base}/kachar_resume_2023.pdf"><Icon class="md:hidden" icon="pepicons-pop:cv" width="24" height="24" /></a>
-    <a class="font-serif md:btn md:btn-ghost md:text-xl hidden" href="{base}/" >Koray G. Kachar</a>
-  </div>
-  <div class="navbar-center md:hidden">
-    <a class="btn btn-ghost font-serif text-xl mx-auto" href="{base}/" >Koray G. Kachar</a>
+    <a class="font-serif btn btn-ghost text-xl" href="{base}/" >Koray G. Kachar</a>
   </div>
   <div class="navbar-end">
     <ul class="menu menu-horizontal px-1 hidden md:flex">
       <li><a href="{base}/kachar_resume_2023.pdf">Resume</a></li>
     </ul>
+    <a href="{base}/kachar_resume_2023.pdf"><Icon class="mr-2 md:hidden" icon="pepicons-pop:cv" width="24" height="24" /></a>
     <div class="md:divider md:divider-horizontal hidden" />
-    <a class="mr-2" href="https://github.com/kachark"><Icon icon="mdi:github" width="24" height="24" /></a>
+    <a class="mx-2" href="https://github.com/kachark"><Icon icon="mdi:github" width="24" height="24" /></a>
     <a class="mx-2" href="https://www.linkedin.com/in/koray-kachar/"><Icon icon="mdi:linkedin" width="24" height="24" /></a>
   </div>
 </div>
